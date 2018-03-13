bd6fced10d0d0ffb445e26824cf9fccdbfa80b3d
jarred <jarredbishop@gmail.com>
2018-03-13 20:34:03 +1300

    update navs

--- _js/src/main.js
+++ _js/src/main.js
@@ -1,19 +1,19 @@
 import Sticky from "sticky-js";
 import Sizzle from "sizzle";
 
-var subnavEl = document.getElementById("subnav");
+var Nav = document.getElementById("Nav");
 var sticky = new Sticky("#subnav");
 
 var toggleNav = function(event) {
   console.log("toggleNav");
-  if (subnavEl.classList.contains("is-open")) {
-    subnavEl.classList.remove("is-open");
-    subnavEl.classList.remove("is-white");
-    document.body.classList.remove("subnav-is-open");
+  if (Nav.classList.contains("is-open")) {
+    Nav.classList.remove("is-open");
+    Nav.classList.remove("is-white");
+    document.body.classList.remove("nav-is-open");
   } else {
-    subnavEl.classList.add("is-open");
-    subnavEl.classList.add("is-white");
-    document.body.classList.add("subnav-is-open");
+    Nav.classList.add("is-open");
+    Nav.classList.add("is-white");
+    document.body.classList.add("nav-is-open");
   }
 };
 
